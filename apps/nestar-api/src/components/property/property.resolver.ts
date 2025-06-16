d19f101415dc08ead9edfa86cbbc998e4c963632
Jackdev0905 <abdurahimovjavohir09@gmail.com>
2025-06-16 22:55:38 +0900

    feat: develop getProperty Business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -2,12 +2,14 @@ import { UseGuards } from '@nestjs/common';
 import { MemberType } from '../../libs/enums/member.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { PropertyService } from './property.service';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Property } from '../../libs/dto/property/property';
 import { PropertyInput } from '../../libs/dto/property/property.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongooseObjectId } from '../../libs/config';
 
 @Resolver()
 export class PropertyResolver {
@@ -21,7 +23,18 @@ export class PropertyResolver {
 		@AuthMember('_id') memberId: ObjectId,
 	): Promise<Property> {
 		console.log('Mutation: createProperty');
-        input.memberId = memberId
+		input.memberId = memberId;
 		return await this.propertyService.createProperty(input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(()=> Property)
+	public async getProperty(
+		@Args('propertyId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Query: getProperty');
+		const propertyId = shapeIntoMongooseObjectId(input);
+		return await this.propertyService.getProperty(memberId, propertyId);
 	}
 }
