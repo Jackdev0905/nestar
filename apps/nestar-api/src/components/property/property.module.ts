a53a95edcbe196ef857160d81e775ba6c5b1502b
Jackdev0905 <abdurahimovjavohir09@gmail.com>
2025-06-16 12:32:24 +0900

    feat: develop createProperty GraphQL api

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -6,9 +6,10 @@ import { PropertyService } from './property.service';
 import { ViewModule } from '../view/view.module';
 import { AuthModule } from '../auth/auth.module';
 import PropertySchema from '../../schemas/Property.model';
+import { MemberModule } from '../member/member.module';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]), AuthModule, ViewModule],
+	imports: [MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]), AuthModule, ViewModule, MemberModule],
   providers: [PropertyResolver, PropertyService]
 })
 export class PropertyModule {}
