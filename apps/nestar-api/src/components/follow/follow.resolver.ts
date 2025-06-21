456102cd1331eaac218f1ddee814fcb776380502
Jackdev0905 <abdurahimovjavohir09@gmail.com>
2025-06-21 20:36:26 +0900

    feat: develop subscribe, unSubscribe, getMemberFollowings and getMemberFollowers APIs

--- apps/nestar-api/src/components/follow/follow.resolver.ts
+++ apps/nestar-api/src/components/follow/follow.resolver.ts
@@ -1,7 +1,61 @@
+import { UseGuards } from '@nestjs/common';
 import { FollowService } from './follow.service';
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Follower, Followers, Followings } from '../../libs/dto/follow/follow';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { shapeIntoMongooseObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 
 @Resolver()
 export class FollowResolver {
 	constructor(private readonly followService: FollowService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => Follower)
+	public async subscribe(
+		@Args('input') input: String,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Follower> {
+		console.log('Mutation: subscribe');
+		const followingId = shapeIntoMongooseObjectId(input)
+		return await this.followService.subscribe(memberId, followingId);
+	}
+
+    @UseGuards(AuthGuard)
+	@Mutation(() => Follower)
+	public async unSubscribe(
+		@Args('input') input: String,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Follower> {
+		console.log('Mutation: unSubscribe');
+		const followingId = shapeIntoMongooseObjectId(input)
+		return await this.followService.unSubscribe(memberId, followingId);
+	}
+
+    @UseGuards(WithoutGuard)
+	@Query(() => Followings)
+	public async getMemberFollowings(
+		@Args('input') input: FollowInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Followings> {
+        const {followerId}= input.search
+        input.search.followerId = shapeIntoMongooseObjectId(followerId)
+		console.log('Query: getMemberFollowings');
+		return await this.followService.getMemberFollowings(memberId, input);
+	}
+
+    @UseGuards(WithoutGuard)
+	@Query(() => Followers)
+	public async getMemberFollowers(
+		@Args('input') input: FollowInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Followers> {
+        const {followingId}= input.search
+        input.search.followingId = shapeIntoMongooseObjectId(followingId)
+		console.log('Query: getMemberFollowers');
+		return await this.followService.getMemberFollowers(memberId, input);
+	}
 }
